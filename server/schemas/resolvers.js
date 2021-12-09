f0cb300d0238abad8836c25bffdd76504940d0dd
leonaysmael16 <ysmealleona@gmail.com>
2021-12-09 02:22:04 -0800

    added code in resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const { AuthenticationError } = require('apollo-server-express');
+const { tokenSign } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const dataUser = await User.findOne({ _id: context.user._id}).select("-_v -password");
+
+                return dataUser;
+            }
+
+            throw new AuthenticationError("You're not in!");
+        }
+
+    },
+    Mutation: {
+        login: async (parent, {email, passwork}) => {
+            const user = await User.findOne({ email });
+        }
+    }
+}
